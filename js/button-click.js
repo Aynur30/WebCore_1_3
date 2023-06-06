731b77f0f4a7dc7041f13f6e58f01c42228886d2
Aynur30 <aynur.shakirov.00.00@mail.ru>
2023-06-06 10:28:32 +0500

    load project

---
+++ js/button-click.js
@@ -0,0 +1,12 @@
+let button=document.querySelectorAll(".slide-menu__item");
+
+    for (let i=0; (i < button.length); i++)
+    button[i].onclick = function() {
+        for (let j=0; (j < button.length); j++) {
+            if (button[j].classList.contains("slide-menu__item_active")) 
+            button[j].classList.remove('slide-menu__item_active');
+        }    
+            if (!button[i].classList.contains("slide-menu__item_active")) 
+            button[i].classList.add('slide-menu__item_active');
+        
+  };
